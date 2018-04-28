e5c5b79bbe128ad2f1a1bf1581d1ec7bb1de1b8d
gunaevart <gunaevart@mail.ru>
2018-04-28 11:43:26 +0300

    Сделал моталку

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,11 @@
+ //slider init
   var slider = document.querySelector('.slider');
   var instance = M.Slider.init(slider);
+//modal init
+  var modHed = document.querySelector('.modal');
+  var modalHeader = M.Modal.init(modHed);
+// Masked phone
+   $("#icon_telephone").mask("(999) 999-9999");
+
 
 
