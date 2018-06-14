dc3a60df07836fffcb7f8fbf2d7a34c70d468c8e
gunaevart <gunaevart@mail.ru>
2018-06-14 13:46:17 +0300

    Сделана page.html

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,15 @@
  //slider init
   var slider = document.querySelector('.slider');
   var instance = M.Slider.init(slider);
+
 //modal init
   var modHed = document.querySelector('.modal');
   var modalHeader = M.Modal.init(modHed);
+
 // Masked phone
    $("#icon_telephone").mask("(999) 999-9999");
+ 
 
-
-
+  // Or with jQuery
+var tabs = document.querySelector('.tabs');
+    var tabsPage = M.Tabs.init(tabs);
